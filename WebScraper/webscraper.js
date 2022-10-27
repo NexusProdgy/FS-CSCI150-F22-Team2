c0079b4b99100850c1ccdc6648969fc4d1b82ca3
RicardoCabrera754 <113645234+RicardoCabrera752@users.noreply.github.com>
2022-10-26 18:42:52 -0700

    Updated webscraper.js
    Changed the webscrpaer.js code so that it only extracts the stream title, streamer name, and category from the Twitch stream URL.

--- WebScraper/webscraper.js
+++ WebScraper/webscraper.js
@@ -25,13 +25,16 @@ async function scrape() {
    
       try{
        await page.goto(link)
+       /*
        var element = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-nxg1ff-0.wEGRY > div", {timeout: 7000})
        var isLive = await page.evaluate(element => element.textContent, element)
-    
+       */
+       /*
        var element2 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-nxg1ff-0.jYkTYc > div.Layout-sc-nxg1ff-0.bBfGll > div.Layout-sc-nxg1ff-0.jUcRho > div > div.Layout-sc-nxg1ff-0.cQQfqe > div.Layout-sc-nxg1ff-0.eLwvPK > div:nth-child(1) > div > p > span")
        var viewCount = await page.evaluate(element2 => element2.textContent, element2)
+       */
     
-       var element3 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-nxg1ff-0.jYkTYc > div.Layout-sc-nxg1ff-0.bBfGll > div.Layout-sc-nxg1ff-0.jFPakw > div > div:nth-child(2) > div > div > div.Layout-sc-nxg1ff-0.FxhJR > a > span")
+       var element3 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-nxg1ff-0.jYkTYc > div.Layout-sc-nxg1ff-0.bBfGll > div.Layout-sc-nxg1ff-0.jFPakw > div > div:nth-child(2) > div > div > div.Layout-sc-nxg1ff-0.FxhJR > a > span", {timeout: 5000})
        var category = await page.evaluate(element3 => element3.textContent, element3)
     
        var element4 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-nxg1ff-0.jYkTYc > div.Layout-sc-nxg1ff-0.bBfGll > div.Layout-sc-nxg1ff-0.jFPakw > div > div.Layout-sc-nxg1ff-0.dRKpYM > h2")
@@ -47,15 +50,15 @@ async function scrape() {
    
      
    
-      console.log(isLive)
-      console.log(viewCount)
+      //console.log(isLive)
+      //console.log(viewCount)
       console.log(category)
       console.log(streamTitle)
       console.log(link)
       console.log(streamerName)
    
    
-      obj = {live: isLive, viewers: viewCount, cat: category, title: streamTitle, URL: link, name: streamerName}
+      obj = {cat: category, title: streamTitle, URL: link, name: streamerName}
    
       myJSON = JSON.stringify(obj)
    
