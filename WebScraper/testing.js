fee7445915d5245309310fcc7bd0596ff7f01a61
RicardoCabrera754 <113645234+RicardoCabrera752@users.noreply.github.com>
2022-12-03 20:20:03 -0800

    Added two testing files to the backend
    Added a testing.js file that I am using to implement the organizing feature of the web scraper.
    Added a file named database-renameCollection.js that can be used to rename collection in the database. This file was added because some collections in the database needed to be renamed to match the category name that can be seen when inspecting the Twitch website.

---
+++ WebScraper/testing.js
@@ -0,0 +1,23 @@
+const categoryList = [];
+categoryList.push("League of Legends")
+categoryList.push("VALORANT")
+categoryList.push("Overwatch 2")
+categoryList.push("Just Chatting")
+
+
+console.log(categoryList[0]);
+console.log(categoryList[1]);
+console.log(categoryList[2]);
+console.log(categoryList[3]);
+
+var found = categoryList.includes("World of Warcraft");
+
+var cat = "Just Chatting"
+
+console.log(found);
+
+if(cat == "Just Chatting"){
+    console.log(true);
+}else{
+    console.log(false);
+}
