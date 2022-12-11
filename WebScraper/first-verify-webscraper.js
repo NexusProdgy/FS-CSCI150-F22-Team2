40f96719031e5160aef53229461b95512f97c855
RicardoCabrera754 <113645234+RicardoCabrera752@users.noreply.github.com>
2022-12-10 19:12:18 -0800

    Added live viewer count verification
    Implemented a web scraper that checks if a stream if live, the stream category, and stream live viewer count. This web scraper verifies streams before they get sent to the main web scraper.
    If a stream is not live then it will not be sent. If the live viewer count of a stream if greater than 100 it will not be sent.
    If a stream has a category that is not listed on the website it will not be sent.

--- WebScraper/first-verify-webscraper.js
+++ WebScraper/first-verify-webscraper.js
@@ -1,65 +1,10 @@
 //Adding External Node.js Modules
 const puppeteer = require('puppeteer') //include puppeteer module
-const fs = require('fs') //include filestream module
-const readline = require('readline') //include readline module
 const {MongoClient} = require('mongodb') //include mongodb module
 
 //const uri = "mongodb+srv://webscraper:csci150@csci150-project-streame.chizztp.mongodb.net/?retryWrites=true&w=majority"; //connection URI to database
 //const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); //create instance of MongoClient using our URI
 
-
-
-//Connect to database using basic if statement
-/*
-client.connect(function (err){
-
-   if(err){
-      console.error("Connection Error");
-   }else{
-      console.error("Connected");
-   }
-
-});
-
-client.close();
-*/
-
-
-
-//Connect to database using try catch block
-/*
-try {
-   client.connect();
-
-
-} catch (err){
-   console.error("Connection Error");
-};
-
-client.close();
-*/
-
-
-//Connect to the database and insert data
-//I'm using this implementation as it works
-//Further testing may be needed
-/*
-client.connect(function(err){
-   if(err) throw err;
-
-   
-   //var myData = {category: "Test", title: "Test Stream", URL: "twitch.tv", name: "Test1"};
-   database.collection("TestCollection").insertOne(data, function(err){
-       if(err) throw err
-       console.log("Insert Success")
-       client.close();
-   });
-   
-
-   //client.close();
-
-});
-*/
 
 const categoryList = [];
 categoryList.push("League of Legends")
@@ -70,45 +15,20 @@ categoryList.push("World of Warcraft")
 categoryList.push("Minecraft")
 
 
+const viewCountRange = 100;
+
+
 //Web Scraper Function
 async function scrape() {
    const browser = await puppeteer.launch({}) //create a new web browser instance
    const page = await browser.newPage() //create page variable that will be used to navigate to Twitch.tv web page
-   const readStream = fs.createReadStream('urlFile.txt') //creating a read stream from a given input file
    const uri = "mongodb+srv://webscraper:csci150@csci150-project-streame.chizztp.mongodb.net/?retryWrites=true&w=majority"; //connection URI to database
    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); //create instance of MongoClient using our URI
 
    const database = client.db("TestDatabase"); //database we are connecting to
    var testCollection = database.collection("TestCollection")
-   /*
-   client.connect(function (err){
-
-      if(err){
-         console.error("Connection Error");
-      }else{
-         console.error("Connected");
-      }
-   
-   });
-   */
    
    
-  
-   //Truncating the output file to delete previous data
-   //Using asynchronous writeFile function
-   /*
-   fs.writeFile('output.txt', '', err => {if (err){ 
-      throw err} console.log(err)})
-         
-       
-     
-   //Reading from the input file one URL at a time
-   //Use crlfDelay to allow program to recognize newline and carriage return
-   const readLine = readline.createInterface({
-      input: readStream,
-      crlfDelay : Infinity
-   })
-   */
 
 
    var projection = {_id: 0, URL: 1}
@@ -123,14 +43,7 @@ async function scrape() {
       //If the streamer is not live then return an error
       try{
        await page.goto(link) //telling the page to navigate to a given Twitch.tv web page for a streamer
-       /*
-       var element = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-nxg1ff-0.wEGRY > div", {timeout: 7000})
-       var isLive = await page.evaluate(element => element.textContent, element)
-       */
        
-       var element2 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-1xcs6mc-0.chGnpF > div.Layout-sc-1xcs6mc-0.bMvWIE > div.Layout-sc-1xcs6mc-0.kFQuPx > div > div.Layout-sc-1xcs6mc-0.fKljfb > div.Layout-sc-1xcs6mc-0.cTzewX > div:nth-child(1) > div > p > span", {timeout: 2900})
-       var viewCount = await page.evaluate(element2 => element2.textContent, element2)
-       console.log("view count", viewCount)
        
     
        //Scraping the stream category
@@ -139,90 +52,73 @@ async function scrape() {
        var element3 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-1xcs6mc-0.chGnpF > div.Layout-sc-1xcs6mc-0.bMvWIE > div.Layout-sc-1xcs6mc-0.elJsHR > div > div:nth-child(2) > div > div > div.Layout-sc-1xcs6mc-0.dlwAAo > a", {timeout: 2900})
        var category = await page.evaluate(element3 => element3.textContent, element3)
 
+
+       var element2 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-1xcs6mc-0.chGnpF > div.Layout-sc-1xcs6mc-0.bMvWIE > div.Layout-sc-1xcs6mc-0.kFQuPx > div > div.Layout-sc-1xcs6mc-0.fKljfb > div.Layout-sc-1xcs6mc-0.cTzewX > div:nth-child(1) > div > p > span")
+       var viewCount = await page.evaluate(element2 => element2.textContent, element2)
+
        if(categoryList.includes(category)){
 
-         console.log("URL:", link,",Category:", category, "Is in database");
-         //Scraping the stream title
-         var element4 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-1xcs6mc-0.chGnpF > div.Layout-sc-1xcs6mc-0.bMvWIE > div.Layout-sc-1xcs6mc-0.elJsHR > div > div.Layout-sc-1xcs6mc-0.BcKcx > h2")
-         var streamTitle = await page.evaluate(element4 => element4.textContent, element4)
 
-         //Scraping the streamer name
-         var element0 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-1xcs6mc-0.chGnpF > div.Layout-sc-1xcs6mc-0.wDxTQ.metadata-layout__support > div.Layout-sc-1xcs6mc-0.beAYWq > a > h1")
-         var streamerName = await page.evaluate(element0 => element0.textContent, element0)
+        if(viewCount <= viewCountRange){
+
+            console.log("URL:", link,",Category:", category, "Is in database");
+            console.log("URL:", link,",View Count:", category, "Is within range");
+            //Scraping the stream title
+            var element4 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-1xcs6mc-0.chGnpF > div.Layout-sc-1xcs6mc-0.bMvWIE > div.Layout-sc-1xcs6mc-0.elJsHR > div > div.Layout-sc-1xcs6mc-0.BcKcx > h2")
+            var streamTitle = await page.evaluate(element4 => element4.textContent, element4)
+
+             //Scraping the streamer name
+            var element0 = await page.waitForSelector("#live-channel-stream-information > div > div > div > div > div.Layout-sc-1xcs6mc-0.chGnpF > div.Layout-sc-1xcs6mc-0.wDxTQ.metadata-layout__support > div.Layout-sc-1xcs6mc-0.beAYWq > a > h1")
+            var streamerName = await page.evaluate(element0 => element0.textContent, element0)
 
 
-         //console.log(isLive)
-         //console.log(viewCount)
-         console.log(category) //print the stream category to the console
-         console.log(streamTitle) //print the stream title to the console
-         console.log(link) //print the stream URL to the console
-         console.log(streamerName) //print the streamer name to the console
+            //console.log(isLive)
+            console.log(viewCount)
+            console.log(category) //print the stream category to the console
+            console.log(streamTitle) //print the stream title to the console
+            console.log(link) //print the stream URL to the console
+            console.log(streamerName) //print the streamer name to the console
 
-         //obj = {category: category, title: streamTitle, URL: link, name: streamerName} //JSON format object
+            //var data = {category: category, title: streamTitle, URL: link, name: streamerName}
 
-         var data = {category: category, title: streamTitle, URL: link, name: streamerName}
+            var data = {URL: link}
 
-         //myJSON = JSON.stringify(obj) //creating the JSON to be sent to the database
 
-         var dbcategory = category.split(" ").join("");
+            //var dbcategory = category.split(" ").join("");
 
-         client.connect(function(err){ //connect to the database
-            if(err) throw err; //if we can't connect then throw error
-    
-            
-            
-            database.collection(dbcategory).insertOne(data, function(err){ //insert the stream properties into the database
-                if(err) throw err //if we can't insert then throw error
-                console.log("Insert Success") //the insert was successful
-                client.close(); //close the connection to the database
-            });
-            
-            
-    
-            //client.close();
-    
-          });
+
+
+            client.connect(function(err){ //connect to the database
+                if(err) throw err; //if we can't connect then throw error
+        
+                
+                
+                database.collection("VerifiedURLCollection").insertOne(data, function(err){ //insert the stream properties into the database
+                    if(err) throw err //if we can't insert then throw error
+                    console.log("Insert Success") //the insert was successful
+                    client.close(); //close the connection to the database
+                });
+                
+                
+        
+                //client.close();
+        
+              });
+
+
+
+
+        }else{
+
+            console.log("Error:", "URL:", link, ",View Count:", viewCount, "Is not within range");
+
+        }
+        
 
        }else{
 
          console.log("Error:", "URL:", link, ",Category:", category, "Is not in database");
        }
-
-
-       
-       //Writing the JSON to the output file
-       //New entries will be appended to the file using newline
-       /*
-       fs.writeFile('output.txt', myJSON + '\n', {flag: 'a'}, err => {if (err){ 
-         throw err} console.log(err)})
-      */
-         /*
-         client.connect(function (err){
-
-            if(err){
-               console.error("Connection Error");
-            }else{
-               console.error("Connected");
-
-
-               
-               testCollection.insertOne(data, function (err){
-                  if(err){
-                     console.log("Insert error")
-                     
-                  }else{
-                     console.log("insert success")
-                  }
-                  
-               });
-               
-               
-            }
-            client.close(); //close the MongoClient instance
-         
-         });
-         */
-         
 
          
    
